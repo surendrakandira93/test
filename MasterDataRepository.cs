a7769a2f0e5da50adbad13bffcfcfe4f599b1e89
Surendra Kandira <surendrakandira@gmail.com>
2018-12-24 18:20:10 +0530

    Repositories
    EngineerPark.Data\Repositories

---
+++ MasterDataRepository.cs
@@ -0,0 +1,34 @@
+﻿using EngineerPark.Data.IRepositories;
+using EngineerPark.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EngineerPark.Data.Repositories
+{
+    public class MasterDataRepository : IMasterDataRepository
+    {
+        private readonly ApplicationDbContext context;
+        public MasterDataRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+
+        }
+        public async Task<List<MasterDataView>> Execute(string sqlQuery)
+        {
+            try
+            {
+
+                var results = await context.MasterDataDB.SqlQuery(sqlQuery).ToListAsync();
+                return results;                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+    }
+}
